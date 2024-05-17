3caa0a767a74a3f6e33f82dc5164e7de7512464c
Elias Morais Santos <ieliasmoraiss@gmail.com>
2024-05-16 21:16:20 -0300

    Update consulta.py

--- consulta.py
+++ consulta.py
@@ -81,9 +81,9 @@ url = "https://raw.githubusercontent.com/Snuking/snuking/main/version.txt"
 version = str(requests.get(url).text)
 
 def main():
-  clear()
-  v = "1.0.3"
-  print(f'''{G}
+   clear()
+   v = "1.0.3"
+   print(f'''{G}
 \n
 .----.______
 |           |
@@ -94,11 +94,11 @@ def main():
 |/__________/ {v}
 {C}
  ''')
-  print(f"{Y}Atual: {version}{C}")
+   print(f"{Y}Atual: {version}{C}")
 
-  print("\n" + code_info + "Menu.")
+   print("\n" + code_info + "Menu.")
 
-  print(f'''
+   print(f'''
 {C}[{G}i{C}] Formas de operação:
 [{G}1{C}] IP.
 [{G}2{C}] Nome.
@@ -117,68 +117,68 @@ def main():
 [{G}00{C}] {R}Sair.{C}
 ''')
 tool=input(f'{C}[{G}+{C}] Selecione a forma de operação:{B} ')
-  if tool == "1":
-     clear()
-     import ip
-     ip.main()
-  elif tool == "2":
-     clear()
-     import nome
-     nome.main()
-  elif tool == "3":
-     clear()
-     import cpf
-     cpf.main()
-  elif tool == "4":
-     clear()
-     import vizinhos
-     vizinhos.main()
-  elif tool == "5":
-     clear()
-     import cep
-     cep.main()
-  elif tool == "6":
-     clear()
-     import cnpj
-     cnpj.main()
-  elif tool == "7":
-     clear()
-     import placa
-     placa.main()
-  elif tool == "8":
-     clear()
-     import telefone
-     telefone.main()
-  elif tool == "9":
-     import bin
-     bin.main()
-  elif tool == "10":
-     import gerador
-     gerador.main()
-  elif tool == "11":
-     youtube()
-     main()
-  elif tool == "44":
-         print(code_result + "Abrindo o tutorial de como atualizar no YouTube...")
-         time.sleep(2)
-         youtube()
-         main()
+   if tool == "1":
+      clear()
+      import ip
+      ip.main()
+   elif tool == "2":
+      clear()
+      import nome
+      nome.main()
+   elif tool == "3":
+      clear()
+      import cpf
+      cpf.main()
+   elif tool == "4":
+      clear()
+      import vizinhos
+      vizinhos.main()
+   elif tool == "5":
+      clear()
+      import cep
+      cep.main()
+   elif tool == "6":
+      clear()
+      import cnpj
+      cnpj.main()
+   elif tool == "7":
+      clear()
+      import placa
+      placa.main()
+   elif tool == "8":
+      clear()
+      import telefone
+      telefone.main()
+   elif tool == "9":
+      import bin
+      bin.main()
+   elif tool == "10":
+      import gerador
+      gerador.main()
+   elif tool == "11":
+      youtube()
+      main()
+   elif tool == "44":
+      print(code_result + "Abrindo o tutorial de como atualizar no YouTube...")
+      time.sleep(2)
+      youtube()
+      main()
 
-elif tool == "55":
-     import novidades
-     novidades.main()
-  elif tool == "66":
-     import ajuda
-     ajuda.main()
-  elif tool == "00":
-     clear()
-     print(f'\n{G}Somos uma legião.{C}\n')
-     exit()
-  else:
-     clear()
-     print(f'{C}[{R}-{C}] Seleção inválida.')
-     time.sleep(0.2)
-     main()
+   elif tool == "55":
+      import novidades
+      novidades.main()
+   elif tool == "66":
+      import ajuda
+      ajuda.main()
+   elif tool == "00":
+      clear()
+      print(f'\n{G}Somos uma legião.{C}\n')
+      exit()
+   else:
+      clear()
+      print(f'{C}[{R}-{C}] Seleção inválida.')
+      time.sleep(0.2)
+      main()
 main()
 
 #Kratosy/Somos uma legião. 2021 ©
